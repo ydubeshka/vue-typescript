a9ceae7c2417676680c410b048ae74e2055f1e37
Yahor Dubeshka <y.dubeshka@gmail.com>
2025-11-03 21:43:10 +0300

    Basics

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
+import {ref, reactive, type Ref} from "vue";
 
+ const likes: Ref<number> = ref(0);
+ const dislikes: Ref<number> = ref(5);
+
+const addLike = () => likes.value++;
+const addDislike = () => dislikes.value++;
 </script>
 
 <template>
-  <div>All is good</div>
+  <div class="like container">
+    <div>Likes count:  <strong>{{ likes }} </strong> </div>
+    <button @click="addLike">like</button>
+  </div>
+  <div class="dislike container">
+    <div>Dislikes count:  <strong>{{ dislikes }} </strong> </div>
+    <button @click="addDislike">Dislike</button>
+  </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+
+.container{
+  display: flex;
+  align-items: center;
+  width: 20vw;
+  justify-content: space-between;
+  margin-bottom: 5px;
 }
 </style>
