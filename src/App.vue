72a96252c6966908fb41bbb11e796bd3997a16fe
Yahor Dubeshka <y.dubeshka@gmail.com>
2025-11-04 16:01:56 +0300

    Dynamic component

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,14 @@ import {CheckCircleIcon} from '@heroicons/vue/24/solid';
 import {ClockIcon, ListBulletIcon, ChartBarIcon} from '@heroicons/vue/24/outline'
 
 
-const navItems: string[] = ['timeline', 'activities', 'progress'];
+
+
+const navItems = {
+  timeline: ClockIcon,
+  activities: ListBulletIcon,
+  progress: ChartBarIcon
+
+};
 
 </script>
 
@@ -34,12 +41,9 @@ const navItems: string[] = ['timeline', 'activities', 'progress'];
 
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="page in navItems" :key="page" class=" flex-1">
+      <li v-for="(icon, page) in navItems" :key="page" class=" flex-1">
         <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize" >
-          <ClockIcon v-if="page === 'timeline'" class="h-6 w-6"/>
-          <ListBulletIcon v-else-if="page === 'activities'" class="h-6 w-6"/>
-          <ChartBarIcon v-else class="h-6 w-6"/>
-          {{ page }}
+          <component :is="icon" class="h-6 w-6"/> {{ page }}
         </a>
       </li>
     </ul>
