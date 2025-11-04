b90468c8f74ddde4646ddaf7afbbfd9e6242bdd7
Yahor Dubeshka <y.dubeshka@gmail.com>
2025-11-04 16:46:21 +0300

    Added active nav items

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {ClockIcon, ListBulletIcon, ChartBarIcon} from '@heroicons/vue/24/outline'
 import NavItem from "./NavItem.vue";
+import {ref} from "vue";
 
 const navItems = {
   timeline: ClockIcon,
@@ -8,12 +9,21 @@ const navItems = {
   progress: ChartBarIcon
 
 };
+
+const currentPage = ref( 'timeline');
+console.log(currentPage);
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+      <NavItem
+          v-for="(icon, page) in navItems"
+          :key="page"
+          :href="`#${page}`"
+          @click="currentPage = page"
+          :class="{'bg-gray-200 pointer-events-none': currentPage === page}"
+      >
         <component :is="icon"  class="h-6 w-6"/> {{ page }}
       </NavItem>
     </ul>
